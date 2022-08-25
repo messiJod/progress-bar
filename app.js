b4d0ca3e70afd20c88fa5911d44fc4d80201fe84
messiJod <messi162108@gmail.com>
2022-08-25 18:24:10 +0530

    progress-bar

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let progressBar = document.querySelector("#progress");
-
+ 
 window.addEventListener("scroll", function () {
   let totalHeight = document.body.clientHeight - window.innerHeight;
   let width = Math.floor((window.pageYOffset / totalHeight) * 100);
