f8e73bbc0f3e6d17a3e42ec5d6849131a6739b85
messiJod <messi162108@gmail.com>
2022-08-25 18:23:09 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let progressBar = document.querySelector("#progress");
+
+window.addEventListener("scroll", function () {
+  let totalHeight = document.body.clientHeight - window.innerHeight;
+  let width = Math.floor((window.pageYOffset / totalHeight) * 100);
+  progressBar.style.width = `${width}%`;
+});
